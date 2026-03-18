8d62d9fd3effcdfb8c33d78a767c5a6e589a05f6
Benjamin Montenegro <benjax.rodeo@gmail.com>
2026-03-18 15:42:37 -0300

    Update storyteller.py

--- storyteller.py
+++ storyteller.py
@@ -5,9 +5,6 @@ from groq import Groq
 load_dotenv()
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-def generate_story(genre, protagonist, premise):
-    prompt = f"Write the first chapter of an story of {genre}. The protagonist is: {protagonist}. The premise is: {premise}."
-
 def main():
     while True:
         user_input = input("""Make a story about: """)
